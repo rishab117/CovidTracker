338af5962755b2e541a6cf5905fd6b2be6e59dc6
Julian Prester <julian@prester.me>
2021-01-22 11:18:17 +1100

    Week 4

--- app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
@@ -1,6 +1,7 @@
 package au.edu.unsw.infs3634.covidtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,19 +9,24 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private CountryAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.btLaunchActivity);
-        button.setOnClickListener(new View.OnClickListener() {
+        mRecyclerView = findViewById(R.id.rvList);
+        mRecyclerView.setHasFixedSize(true);
+        CountryAdapter.RecyclerViewClickListener listener = new CountryAdapter.RecyclerViewClickListener() {
             @Override
-            public void onClick(View v) {
-                launchDetailActivity("40735414-f500-4ba6-a05f-375fb7e93db8");
+            public void onClick(View view, String id) {
+                launchDetailActivity(id);
             }
-        });
+        };
+        mAdapter = new CountryAdapter(Country.getCountries(), listener);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     private void launchDetailActivity(String message) {
